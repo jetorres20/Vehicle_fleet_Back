01fdde876ad53878fdc3673a34260b93ec601a51
Mateo Laguna Guantiva <m.laguna10@uniandes.edu.co>
2020-02-18 20:57:53 -0500

    Last test in Reserva was succesful

--- s1_automotor-back/src/test/java/co/edu/uniandes/csw/automotor/test/logic/ReservaLogicTest.java
+++ s1_automotor-back/src/test/java/co/edu/uniandes/csw/automotor/test/logic/ReservaLogicTest.java
@@ -5,14 +5,17 @@
  */
 package co.edu.uniandes.csw.automotor.test.logic;
 
+import co.edu.uniandes.csw.automotor.ejb.RegistroLogic;
 import co.edu.uniandes.csw.automotor.ejb.ReservaLogic;
 import co.edu.uniandes.csw.automotor.ejb.TipoVehiculoLogic;
+import co.edu.uniandes.csw.automotor.ejb.VehiculoLogic;
 import co.edu.uniandes.csw.automotor.entities.RegistroEntity;
 import co.edu.uniandes.csw.automotor.entities.ReservaEntity;
 import co.edu.uniandes.csw.automotor.entities.TipoVehiculoEntity;
 import co.edu.uniandes.csw.automotor.entities.VehiculoEntity;
 import co.edu.uniandes.csw.automotor.exceptions.BusinessLogicException;
 import co.edu.uniandes.csw.automotor.persistence.ReservaPersistence;
+import co.edu.uniandes.csw.automotor.persistence.VehiculoPersistence;
 import java.util.Date;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
@@ -48,7 +51,13 @@ public class ReservaLogicTest {
     private PodamFactory factory = new PodamFactoryImpl();
     
     @Inject
-    private ReservaLogic reservaLogic;   
+    private ReservaLogic reservaLogic;
+    
+    @Inject
+    private VehiculoLogic vehiculoLogic;
+    
+    @Inject
+    private RegistroLogic registroLogic;
     
     @PersistenceContext
     private EntityManager em;
@@ -98,26 +107,23 @@ public class ReservaLogicTest {
     public void createReservaDuplicadas() throws BusinessLogicException
     {
         ReservaEntity newEntity = factory.manufacturePojo(ReservaEntity.class);
-        ReservaEntity newEntity2 = factory.manufacturePojo(ReservaEntity.class);
-        
+        VehiculoEntity newVehiculo = factory.manufacturePojo(VehiculoEntity.class);
+        RegistroEntity newRegistro = factory.manufacturePojo(RegistroEntity.class);
         newEntity.setFechaReserva(new Date(2020, 2, 4, 14, 30));
         newEntity.setFechaServicio(new Date(2020, 6, 4, 13, 30));
+        registroLogic.createRegistro(newRegistro);
+        newVehiculo.setRegistro(newRegistro);
+        vehiculoLogic.createVehiculo(newVehiculo);
+        newEntity.setVehiculo(newVehiculo);
+            
+        ReservaEntity result = reservaLogic.createReservaContext(newEntity);
         
+        ReservaEntity newEntity2 = factory.manufacturePojo(ReservaEntity.class);
         newEntity2.setFechaReserva(new Date(2020, 2, 4, 14, 30));
         newEntity2.setFechaServicio(new Date(2020, 6, 4, 13, 30));
+        newEntity2.setVehiculo(newVehiculo);
         
-        VehiculoEntity car = new VehiculoEntity();
-        car.setMarca("BMW");
-        car.setModelo("2020");
-        car.setPlaca("AXW390");
-        car.setCapacidad(4);
-        car.setRegistro(new RegistroEntity());
-        newEntity.setVehiculo(car);
-        newEntity2.setVehiculo(car);
-        
-        ReservaPersistence pe = reservaLogic.getRservaPersitence();
-        ReservaEntity result = reservaLogic.createReserva(newEntity);
-        pe.create(newEntity);
         ReservaEntity result2 = reservaLogic.createReserva(newEntity2);
     }
+    
 }
