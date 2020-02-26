eb2323a1af3baffa3d405021480faef7d342470c
Mateo Laguna Guantiva <m.laguna10@uniandes.edu.co>
2020-02-25 21:17:17 -0500

    Bug fixed in ConductorLogicTest

---
+++ s1_automotor-api/src/main/java/co/edu/uniandes/csw/automotor/dtos/TipoVehiculoDTO.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.automotor.dtos;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Mateo Laguna G.
+ */
+public class TipoVehiculoDTO implements Serializable{
+    
+}
