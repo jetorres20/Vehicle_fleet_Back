ebcf45da0d02f22fdba8d94dc5cbbe431ecce452
Mateo Laguna Guantiva <m.laguna10@uniandes.edu.co>
2020-02-26 08:25:46 -0500

    TipoVehiculoResource and ReservaResource added.

--- s1_automotor-api/src/main/java/co/edu/uniandes/csw/automotor/dtos/TipoVehiculoDTO.java
+++ s1_automotor-api/src/main/java/co/edu/uniandes/csw/automotor/dtos/TipoVehiculoDTO.java
@@ -5,7 +5,10 @@
  */
 package co.edu.uniandes.csw.automotor.dtos;
 
+import co.edu.uniandes.csw.automotor.entities.TipoVehiculoEntity;
 import java.io.Serializable;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 
 /**
  *
@@ -14,4 +17,56 @@ import java.io.Serializable;
 public class TipoVehiculoDTO implements Serializable{
     
     private String tipo;
+    
+    /**
+     * Constructor por defecto
+     */
+    public TipoVehiculoDTO() {
+    }
+    
+    /**
+     * Conviertir Entity a DTO (Crea un nuevo DTO con los valores que recibe en
+     * la entidad que viene de argumento.
+     *
+     * @param editorialEntity: Es la entidad que se va a convertir a DTO
+     */
+    public TipoVehiculoDTO(TipoVehiculoEntity tipoVehiculoEntity) {
+        if(tipoVehiculoEntity != null) {
+            this.tipo = tipoVehiculoEntity.getTipo();
+        }
+    }
+    
+    /**
+     * Devuelve el tipo del TipoVehiculo.
+     *
+     * @return el tipo
+     */
+    public String getTipo() {
+        return this.tipo;
+    }
+    
+    /**
+     * Modifica el tipo de TipoVehiculo.
+     *
+     * @param tipo the id to set
+     */
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    
+    /**
+     * Convertir DTO a Entity
+     *
+     * @return Un Entity con los valores del DTO
+     */
+    public TipoVehiculoEntity toEntity() {
+        TipoVehiculoEntity tipoVehiculoEntity = new TipoVehiculoEntity();
+        tipoVehiculoEntity.setTipo(this.tipo);
+        return tipoVehiculoEntity;
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+    }
 }
