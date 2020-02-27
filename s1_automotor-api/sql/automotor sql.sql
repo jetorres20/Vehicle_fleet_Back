7238cd146d18b61566f574f50dafe3b5f226e431
Juan Andres Caycedo S <ja.caycedo@uniandes.edu.co>
2020-02-27 10:30:38 -0500

    Pruebas mecanicas postman vehiculo y registro

---
+++ s1_automotor-api/sql/automotor sql.sql
@@ -0,0 +1,12 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  estudiante
+ * Created: 27-feb-2020
+ */
+
+delete from RegistroEntity;
+delete from VehiculoEntity;
