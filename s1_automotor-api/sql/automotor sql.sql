974965ffbc6e66a6cefa37b3db8d3abf8bc8c58b
Juan Andres Caycedo S <ja.caycedo@uniandes.edu.co>
2020-04-10 18:19:26 -0500

    sql

--- s1_automotor-api/sql/automotor sql.sql
+++ s1_automotor-api/sql/automotor sql.sql
@@ -21,3 +21,9 @@ delete from EncuestaEntity;
 delete from ESTUDIANTEENTITY_PROFESORENTITY;
 delete from RESERVAENTITY_ESTUDIANTEENTITY;
 delete from ConductorEntity;
+
+INSERT INTO REGISTROENTITY (ID,PRSC,PRSE,RTM,SOAT,VIGENTE)
+VALUES(216,'2038-01-19 00:00:00','2038-01-19 00:00:00','2038-01-19 00:00:00','2038-01-19 00:00:00',1);
+
+INSERT INTO VEHICULOENTITY(id,capacidad,marca,modelo,placa,registro_id)
+values(216,4,'Renault','logan','KIS412',216);
